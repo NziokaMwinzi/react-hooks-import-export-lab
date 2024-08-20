6cb8389a09b9345b39f25a504e7b95f6e4bfaa26
daniel nzioka <dmwinzi50@gmail.com>
2024-08-20 17:59:18 +0300

    import n export

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,13 @@
 import React from "react";
+import * as media from "../data/user"
 
 function Home() {
   return (
     <div id="home">
       <h1>
-        {username} is a Web Developer from {city}
+        {media.username} is a Web Developer from {media.city}
       </h1>
     </div>
   );
 }
+export default Home
